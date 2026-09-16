bc5dde54948f3ffbb78b957dddb53659bf8e4669
danil <kochura2017@yandex.ru>
2026-09-16 12:47:41 +0300

    Сборка макета

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,55 @@
 <template>
-  <main />
+  <div class="shell">
+    <div class="tmp">Шапка?</div>
+
+    <main class="grid">
+      <LeftBar></LeftBar>
+      <div class="col-main">
+        <Map></Map>
+        <div class="tmp">
+          Диаграммы Ганта
+        </div>
+      </div>
+    </main>
+
+  </div>
 </template>
+<script setup lang="ts">
+import Map from "./components/Map.vue";
+import LeftBar from "./components/LeftBar.vue";
+</script>
+
+<style scoped>
+.shell {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  min-height: 0;
+}
+.grid {
+  flex: 1;
+  min-height: 0;
+  display: grid;
+  grid-template-columns: 330px minmax(0, 1fr);
+  gap: 8px;
+  padding: 8px;
+}
+.col-main {
+  display: grid;
+  grid-template-rows: minmax(0, 1.55fr) minmax(0, 1fr);
+  gap: 8px;
+  min-height: 0;
+}
+
+.tmp {
+  background: rgba(0, 255, 255, 0.2);
+  border: 3px dashed #2a4893;
+  padding: 10px;
+  border-radius: 25px;
+  display: flex;
+  align-items: center;
+  align-content: center;
+  justify-content: center;
+  font-size: larger;
+}
+</style>
---
+++ frontend/src/components/LeftBar.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+</script>
+
+<template>
+  <aside class="side panel">
+    <nav class="tabs">
+
+      <button>Бригады</button>
+    </nav>
+
+    <div>
+      <div v-for="i in 15" class="brigada">
+        <div class="brigada-title"> Бригада {{i}}</div>
+      </div>
+    </div>
+
+  </aside>
+
+</template>
+
+<style scoped>
+.brigada {
+  padding: 10px;
+  border: 1px dotted gray;
+  margin: 10px 0px;
+}
+
+</style>
